527951c17b2a120cce9afee3cbfa5bc31517b6dc
WTH 30 <1353962205@qq.com>
2020-12-04 16:06:21 +0800

    更新配置文件

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ module.exports = {
         // 生成的 token 有效时间为 24 小时
         exp: 24 * 60 * 60,
         // 使用你自己的密钥，用来根据密钥生成 token
-        secretKey: 'your_own_secretKey'
+        secretKey: 'WTH30haha'
     },
     // 文件上传用到了七牛云的对象存储服务，详情访问官网 https://developer.qiniu.com/kodo/sdk/1289/nodejs
     upload: {
         // 下面的参数必须声明
-        domain: '',
-        bucket: '',
-        accessKey: '',
-        secretKey: ''
+        domain: 'ms.reddme.com',
+        bucket: 'ms-todo',
+        accessKey: 'wC1ggwXnmOjrYH_KafOUHMjNocGMw2UmkHUJrBgB',
+        secretKey: 'L99sb4g33VADVtoC-gLqbN_0gqr0c-9PPxm8jPyT'
     },
     // 你的发件邮箱服务器配置
     email: {
